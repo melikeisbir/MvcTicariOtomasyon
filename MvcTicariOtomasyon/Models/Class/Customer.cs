2e89e29c47499deb08f31587586da6acf219402f
Melike İsbir <melikeisbr@gmail.com>
2024-03-01 15:19:10 +0300

    customer and employee classes

---
+++ MvcTicariOtomasyon/Models/Class/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Customer
+    {
+        [Key]
+        public int MusteriID { get; set; }
+        public string MusteriAd { get; set; }
+        public string MusteriSoyad { get; set; }
+        public string MusteriSehir { get; set; }
+        public string MusteriMail { get; set; }
+    }
+}
---
+++ MvcTicariOtomasyon/Models/Class/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Employee
+    {
+        [Key]
+        public int PersonelID { get; set; }
+        public string PersonelAd { get; set; }
+        public string PersonelSoyad { get; set; }
+        public string PersonelGorsel { get; set; }
+    }
+}
