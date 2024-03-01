ba146c12b3db97bc6a89ba5a3352f22dd85856b1
Melike İsbir <melikeisbr@gmail.com>
2024-03-01 15:01:21 +0300

    Add project files.

---
+++ MvcTicariOtomasyon/Models/Class/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Category
+    {
+        [Key]
+        public int KategoriID { get; set; }
+        public string KategoriAd { get; set; }
+    }
+}
---
+++ MvcTicariOtomasyon/Models/Class/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Product
+    {
+        [Key]
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string UrunMarka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set;}
+        public decimal SatisFiyat { get; set;}
+        public bool Durum { get; set;}
+        public string UrunGorsel { get; set;}
+    }
+}
