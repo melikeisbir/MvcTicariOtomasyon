f8af3cbdf05775743d649a559e58bf80119ed6ff
Melike İsbir <melikeisbr@gmail.com>
2024-03-01 19:47:57 +0300

    invoice and department classes

---
+++ MvcTicariOtomasyon/Models/Class/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmanID { get; set; }
+        public string DepartmanAd { get; set; }
+    }
+}
---
+++ MvcTicariOtomasyon/Models/Class/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Invoice
+    {
+        [Key]
+        public int FaturaID { get; set; }
+        public string FaturaSeriNo { get; set; }
+        public string FaturaSiraNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public string VergiDairesi { get; set; }
+        public DateTime Saat { get; set; }
+        public string TeslimEden { get; set; }
+        public string TeslimAlan { get; set; }
+    }
+}
