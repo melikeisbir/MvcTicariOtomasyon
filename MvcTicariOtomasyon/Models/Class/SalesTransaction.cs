299387ab846ac7292c35237a6ca08b6f677a94a8
Melike İsbir <melikeisbr@gmail.com>
2024-03-01 20:01:01 +0300

    sales transaction class

---
+++ MvcTicariOtomasyon/Models/Class/SalesTransaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class SalesTransaction
+    {
+        [Key] 
+        public int SatisID { get; set; }
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+    }
+}
