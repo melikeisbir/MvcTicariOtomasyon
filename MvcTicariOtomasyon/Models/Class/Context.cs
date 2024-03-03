04dbc1c950233c802a8fbaa134a29dff64605bbb
Melike İsbir <melikeisbr@gmail.com>
2024-03-03 19:48:35 +0300

    installation of packages, context

---
+++ MvcTicariOtomasyon/Models/Class/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SalesTransaction> SalesTransactions { get; set; }
+    }
+}
