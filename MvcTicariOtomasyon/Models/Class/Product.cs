d2f421dcdecd8eeb6394903010a4daeca105d4e0
Melike İsbir <melikeisbr@gmail.com>
2024-03-08 19:10:27 +0300

    update

--- MvcTicariOtomasyon/Models/Class/Product.cs
+++ MvcTicariOtomasyon/Models/Class/Product.cs
@@ -11,20 +11,21 @@ namespace MvcTicariOtomasyon.Models.Class
     {
         [Key]
         public int UrunID { get; set; }
-        [Column(TypeName ="Varchar")]
+        [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string UrunAd { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string UrunMarka { get; set; }
         public short Stok { get; set; }
-        public decimal AlisFiyat { get; set;}
-        public decimal SatisFiyat { get; set;}
-        public bool Durum { get; set;}
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
-        public string UrunGorsel { get; set;}
-        public virtual Category Category { get; set; }
+        public string UrunGorsel { get; set; }
+        public int? KategoriId { get; set; }
+        public  Category Category { get; set; }
         public ICollection<SalesTransaction> SalesTransactions { get; set; }
 
     }
