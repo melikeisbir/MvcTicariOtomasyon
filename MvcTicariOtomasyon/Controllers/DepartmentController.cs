eba8960d37c2d7b4fe12400015f098186f0fb850
Melike İsbir <melikeisbr@gmail.com>
2024-03-08 20:01:18 +0300

    department index

---
+++ MvcTicariOtomasyon/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departments.ToList();
+            return View(degerler);
+        }
+    }
+}
