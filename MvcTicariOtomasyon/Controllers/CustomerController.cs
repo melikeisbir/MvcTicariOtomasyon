83fe1db5a070f876dc48856567b0e79507aaed7b
Melike İsbir <melikeisbr@gmail.com>
2024-03-09 19:26:32 +0300

    listing customer

---
+++ MvcTicariOtomasyon/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Customers.ToList();
+            return View(degerler);
+        }
+    }
+}
