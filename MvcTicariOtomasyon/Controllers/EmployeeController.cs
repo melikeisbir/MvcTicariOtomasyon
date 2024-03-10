bc7b12ad57539a78f04a748186af267ae802c3f4
Melike İsbir <melikeisbr@gmail.com>
2024-03-10 19:21:26 +0300

    listing employee

---
+++ MvcTicariOtomasyon/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Employees.ToList();
+            return View(degerler);
+        }
+    }
+}
