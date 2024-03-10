6ca0986fb5ac57c1fa3be4685da561a698ba324f
Melike İsbir <melikeisbr@gmail.com>
2024-03-10 19:29:06 +0300

    adding new employee

--- MvcTicariOtomasyon/Controllers/EmployeeController.cs
+++ MvcTicariOtomasyon/Controllers/EmployeeController.cs
@@ -16,5 +16,24 @@ namespace MvcTicariOtomasyon.Controllers
             var degerler = c.Employees.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult PersonelEkle() 
+        {
+            List<SelectListItem> deger1 = (from x in c.Departments.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Employee p)
+        {
+            c.Employees.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
