ba7a91c109404f50dd0a6e2dab09571b7fd440db
Melike İsbir <melikeisbr@gmail.com>
2024-03-12 14:14:39 +0300

    sale page

---
+++ MvcTicariOtomasyon/Controllers/SaleController.cs
@@ -0,0 +1,32 @@
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Sale
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SalesTransactions.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SalesTransaction s)
+        {
+            c.SalesTransactions.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
