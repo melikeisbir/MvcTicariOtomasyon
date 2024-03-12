acaf1c549038eb9cc46517f4a097c0b95410e308
Melike İsbir <melikeisbr@gmail.com>
2024-03-12 14:30:50 +0300

    making a new sale

--- MvcTicariOtomasyon/Controllers/SaleController.cs
+++ MvcTicariOtomasyon/Controllers/SaleController.cs
@@ -18,12 +18,37 @@ namespace MvcTicariOtomasyon.Controllers
         }
         [HttpGet]
         public ActionResult YeniSatis()
-        {
+        {//dropdown ile çekebilmemiz için listeleme komutları
+            List<SelectListItem> deger1 = (from x in c.Products.ToList() //ürünler
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.UrunID.ToString()
+                                           }).ToList();
+
+
+            List<SelectListItem> deger2 = (from x in c.Customers.ToList() //cariler
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Employees.ToList()  //personeller
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         [HttpPost]
         public ActionResult YeniSatis(SalesTransaction s)
         {
+            s.Tarih = DateTime.Parse(DateTime.Now.ToShortTimeString());
             c.SalesTransactions.Add(s);
             c.SaveChanges();
             return RedirectToAction("Index");
