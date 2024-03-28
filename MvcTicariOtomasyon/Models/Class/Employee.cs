624edd5c71ca33cdc347916df2dc1ba7c5a932ba
Melike İsbir <melikeisbr@gmail.com>
2024-03-28 14:51:08 +0300

    display usage, retrieving employee images

--- MvcTicariOtomasyon/Models/Class/Employee.cs
+++ MvcTicariOtomasyon/Models/Class/Employee.cs
@@ -11,14 +11,17 @@ namespace MvcTicariOtomasyon.Models.Class
     {
         [Key]
         public int PersonelID { get; set; }
+        [Display(Name ="Personel Adı")]
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string PersonelAd { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Display(Name = "Personel Soyadı")]
         public string PersonelSoyad { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
+        [Display(Name = "Görsel")]
         public string PersonelGorsel { get; set; }
         public ICollection<SalesTransaction> SalesTransactions { get; set; }
         public int DepartmanID { get; set; }
