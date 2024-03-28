2dc156e08c384726a768dbcc22701aa5673c4060
Melike İsbir <melikeisbr@gmail.com>
2024-03-28 14:36:14 +0300

    to do list cards

--- MvcTicariOtomasyon/Controllers/ToDoListController.cs
+++ MvcTicariOtomasyon/Controllers/ToDoListController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,17 @@ namespace MvcTicariOtomasyon.Controllers
     public class ToDoListController : Controller
     {
         // GET: ToDoList
+        Context c = new Context();
         public ActionResult Index()
         {
+            var deger1 = c.Customers.Count().ToString();
+            ViewBag.d1 = deger1;
+            var deger2 = c.Products.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Categories.Count().ToString();
+            ViewBag.d3 = deger3;
+            var deger4 = (from x in c.Customers select x.CariSehir).Distinct().Count().ToString();
+            ViewBag.d4 = deger4;
             return View();
         }
     }
