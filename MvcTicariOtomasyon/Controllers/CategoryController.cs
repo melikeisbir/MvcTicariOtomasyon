90840cc2d6db8d5a2584cbca8db350c8219becc5
Melike İsbir <melikeisbr@gmail.com>
2024-04-04 15:29:04 +0300

    paging on category page

--- MvcTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcTicariOtomasyon/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using MvcTicariOtomasyon.Models.Class;
+using PagedList;
+using PagedList.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,9 @@ namespace MvcTicariOtomasyon.Controllers
     {
         // GET: Category
         Context c = new Context();
-        public ActionResult Index()
+        public ActionResult Index(int sayfa = 1)
         {
-            var degerler = c.Categories.ToList();
+            var degerler = c.Categories.ToList().ToPagedList(sayfa, 4);
             return View(degerler);
         }
         [HttpGet]
@@ -43,7 +45,7 @@ namespace MvcTicariOtomasyon.Controllers
         public ActionResult KategoriGuncelle(Category k)
         {
             var ktgr = c.Categories.Find(k.KategoriID);
-            ktgr.KategoriAd=k.KategoriAd;
+            ktgr.KategoriAd = k.KategoriAd;
             c.SaveChanges();
             return RedirectToAction("Index");
         }
