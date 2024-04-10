306bddd40fecf15f53387c7b1c7cdcdbf0a0a1c6
Melike İsbir <melikeisbr@gmail.com>
2024-04-11 00:24:45 +0300

    cargo detail page

---
+++ MvcTicariOtomasyon/Controllers/CargoController.cs
@@ -0,0 +1,20 @@
+﻿using MvcTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTicariOtomasyon.Controllers
+{
+    public class CargoController : Controller
+    {
+        // GET: Cargo
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var kargolar = c.CargoDetails.ToList();
+            return View(kargolar);
+        }
+    }
+}
