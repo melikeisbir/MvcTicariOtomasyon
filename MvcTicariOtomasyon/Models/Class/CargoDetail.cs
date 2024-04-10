767820c43589d5af2b39c080f718938e667b97f1
Melike İsbir <melikeisbr@gmail.com>
2024-04-11 00:01:17 +0300

    cargo detail class

---
+++ MvcTicariOtomasyon/Models/Class/CargoDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTicariOtomasyon.Models.Class
+{
+    public class CargoDetail
+    {
+        public int KargoDetayID { get; set; }
+        public string Aciklama { get; set; }
+        public string TakipKodu { get; set; }
+        public string Personel { get; set; }
+        public string Alici { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
