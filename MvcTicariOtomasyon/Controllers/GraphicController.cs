7ae7068ede61a18f2c8d8c6ac894232b47e4c589
Melike İsbir <melikeisbr@gmail.com>
2024-04-15 18:58:06 +0300

    creating a chart in the controller

--- MvcTicariOtomasyon/Controllers/GraphicController.cs
+++ MvcTicariOtomasyon/Controllers/GraphicController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MvcTicariOtomasyon.Controllers
@@ -12,6 +13,12 @@ namespace MvcTicariOtomasyon.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Index2()
+        {
+            var grafikciz = new Chart(600, 600);
+            grafikciz.AddTitle("Kategori - Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler", xValue: new[] { "Molbilya", "Ofis Eşyaları", "Bilgisayar" }, yValues: new[] { 85, 66, 98 }).Write();
+            return File(grafikciz.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
